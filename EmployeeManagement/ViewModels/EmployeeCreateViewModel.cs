844d0219943f64f53b570ce319d2429c6b5dcd1d
Mario Syti <msyti@outlook.com>
2021-08-22 04:20:07 +0300

    added photopath property,created viewmodel

--- EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace EmployeeManagement.Models
+
+namespace EmployeeManagement.ViewModels
 {
-    public class Employee
+    public class EmployeeCreateViewModel
     {
-        public int Id { get; set; }
         [Required]
         [MaxLength(50, ErrorMessage = "Name cannot exceed  50 characters")]
         public string Name { get; set; }
@@ -19,6 +17,6 @@ namespace EmployeeManagement.Models
         public string Email { get; set; }
         [Required]
         public Dept? Department { get; set; }
-        public string PhotoPath { get; set; }
+        public IFormFile Photo { get; set; }
     }
 }
